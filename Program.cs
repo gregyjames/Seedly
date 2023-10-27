16b77275f65dc47a808763974e34863c4d423ec6
Greg James <gregvjames99@gmail.com>
2023-10-27 15:44:02 -0500

    Got server working

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ using Seedly;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { });
 builder.Services.AddMudServices();
 await builder.Build().RunAsync();
--- SeedlyServer/Program.cs
+++ SeedlyServer/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Grpc.Core;
+using LibSeedy;
 
 const int Port = 50051;
 
@@ -17,15 +18,3 @@ Console.WriteLine("Press any key to stop the server...");
 Console.ReadKey();
 
 server.ShutdownAsync().Wait();
-
-public class DownloadService : WeatherForecast.WeatherForecasts.WeatherForecastsBase
-{
-    public override async Task GetUpdateStream(WeatherForecast.DownloadRequest request, IServerStreamWriter<WeatherForecast.Update> responseStream, ServerCallContext context)
-    {
-        for (int i = 1; i <= 1000; i++)
-        {
-            await responseStream.WriteAsync(new WeatherForecast.Update() { ProgressInt = i });
-            await Task.Delay(TimeSpan.FromSeconds(1)); // Simulate some processing time
-        }
-    }
-}
