b6f9b1aaf1479aa02c03b8f542113683a1a9fe7e
Greg James <gregvjames99@gmail.com>
2023-10-27 04:58:47 -0500

    Added seedy server

---
+++ SeedlyServer/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Core;
+
+const int Port = 50051;
+
+Server server = new Server
+{
+    Services = { WeatherForecast.WeatherForecasts.BindService(new DownloadService()) },
+    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+};
+
+server.Start();
+
+Console.WriteLine("Server listening on port " + Port);
+Console.WriteLine("Press any key to stop the server...");
+Console.ReadKey();
+
+server.ShutdownAsync().Wait();
+
+public class DownloadService : WeatherForecast.WeatherForecasts.WeatherForecastsBase
+{
+    public override async Task GetUpdateStream(WeatherForecast.DownloadRequest request, IServerStreamWriter<WeatherForecast.Update> responseStream, ServerCallContext context)
+    {
+        for (int i = 1; i <= 1000; i++)
+        {
+            await responseStream.WriteAsync(new WeatherForecast.Update() { ProgressInt = i });
+            await Task.Delay(TimeSpan.FromSeconds(1)); // Simulate some processing time
+        }
+    }
+}
